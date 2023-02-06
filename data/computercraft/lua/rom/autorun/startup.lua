b6855075591113d349caaa35a577f97532d42687
Rutger van der Eijk <rutger.van.der.eijk@real8.nl>
2023-02-06 17:26:58 +0100

    Move remaining files

--- data/computercraft/lua/rom/autorun/startup.lua
+++ data/computercraft/lua/rom/autorun/startup.lua
@@ -1,3 +1,9 @@
+
+--for n in pairs(_G) do print(n) end
+package.path = package.path..";/rom/modules/ccwp/?;/rom/modules/ccwp/core/?;/rom/modules/ccwp/obj/?;/rom/modules/ccwp/role/?;/rom/modules/ccwp/mobj/?;/rom/modules/ccwp/enterprise/?"
+
+local core = require "core"
+
 -- initialize core modules
 core.Init()
 
