1756554db4232a3f1fcea39a248e2ea71a1a2b50
Rutger van der Eijk <rutger.van.der.eijk@real8.nl>
2023-02-09 20:19:05 +0100

    Improve role setup (incl tests)

--- data/computercraft/lua/rom/modules/ccwp/role/library.lua
+++ data/computercraft/lua/rom/modules/ccwp/role/library.lua
@@ -9,12 +9,18 @@ function library.Setup()
     -- register library objects
     local ObjectRegistry = require "object_registry"
     local objectRegistry = ObjectRegistry:getInstance()
+    objectRegistry:requireAndRegisterObject("role_alchemist")
+    objectRegistry:requireAndRegisterObject("role_builder")
+    objectRegistry:requireAndRegisterObject("role_chests_worker")
+    objectRegistry:requireAndRegisterObject("role_forester")
+    objectRegistry:requireAndRegisterObject("role_fuel_worker")
     objectRegistry:requireAndRegisterObject("role_settler")
-    -- ToDo: add others
+    objectRegistry:requireAndRegisterObject("role_storage_silo_worker")
 
     -- register library object tests
     objectRegistry:requireAndRegisterObject("t_alchemist", "test.t_alchemist")
     objectRegistry:requireAndRegisterObject("t_builder", "test.t_builder")
+    objectRegistry:requireAndRegisterObject("t_foresting", "test.t_foresting")
 
     -- do other stuff
 end
