89fad6a20fe110e2bd944907affddb5df77dbd7c
Rutger van der Eijk <rutger.van.der.eijk@real8.nl>
2023-02-10 15:36:11 +0100

    Generalise ObjectFactory
    - add register and de register methods
    - make it into an object itself

--- data/computercraft/lua/rom/modules/ccwp/mobj/library.lua
+++ data/computercraft/lua/rom/modules/ccwp/mobj/library.lua
@@ -6,11 +6,15 @@ function library.Init()
 end
 
 function library.Setup()
+    -- register library classes
+    local ObjectFactory = require "object_factory"
+    local objectFactory = ObjectFactory:getInstance()
+    objectFactory:registerClass("Chest", require "mobj_chest")
+
     -- register library modules
     local ModuleRegistry = require "module_registry"
     local moduleRegistry = ModuleRegistry:getInstance()
-    moduleRegistry:requireAndRegisterModule("Chest", "mobj_chest")
-    -- ToDo: consider adding & using object_factory
+    moduleRegistry:requireAndRegisterModule("Chest", "mobj_chest") -- ToDo: refactor Chest to no longer need to register it also as a module
 
     -- register library modules test modules
     moduleRegistry:requireAndRegisterModule("T_Chest", "test.t_mobj_chest")
--- data/computercraft/lua/rom/modules/ccwp/obj/library.lua
+++ data/computercraft/lua/rom/modules/ccwp/obj/library.lua
@@ -6,12 +6,17 @@ function library.Init()
 end
 
 function library.Setup()
-    -- register library modules
-    local ModuleRegistry = require "module_registry"
-    local moduleRegistry = ModuleRegistry:getInstance()
-    -- ToDo: add
+    -- register library classes
+    local ObjectFactory = require "object_factory"
+    local objectFactory = ObjectFactory:getInstance()
+    objectFactory:registerClass("URL", require "obj_url")
+    objectFactory:registerClass("Block", require "obj_block")
+    objectFactory:registerClass("LayerRectangle", require "obj_layer_rectangle")
+    objectFactory:registerClass("ItemInventory", require "obj_item_inventory")
 
     -- register library modules test modules
+    local ModuleRegistry = require "module_registry"
+    local moduleRegistry = ModuleRegistry:getInstance()
     moduleRegistry:requireAndRegisterModule("T_ModuleRegistry", "test.t_module_registry")
     moduleRegistry:requireAndRegisterModule("T_ObjectFactory", "test.t_object_factory")
     moduleRegistry:requireAndRegisterModule("T_URL", "test.t_obj_url")
