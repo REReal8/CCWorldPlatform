3c94507a807c246b31a75b09fb1a264fd6e42ec9
Rutger van der Eijk <rutger.van.der.eijk@real8.nl>
2023-02-10 14:30:22 +0100

    Add T_ModuleRegistry and T_ObjectFactory to registry

--- data/computercraft/lua/rom/modules/ccwp/obj/library.lua
+++ data/computercraft/lua/rom/modules/ccwp/obj/library.lua
@@ -6,12 +6,14 @@ function library.Init()
 end
 
 function library.Setup()
-    -- register library objects
+    -- register library modules
     local ModuleRegistry = require "module_registry"
     local moduleRegistry = ModuleRegistry:getInstance()
     -- ToDo: add
 
-    -- register library object tests
+    -- register library module tests
+    moduleRegistry:requireAndRegisterObject("T_ModuleRegistry", "test.t_module_registry")
+    moduleRegistry:requireAndRegisterObject("T_ObjectFactory", "test.t_object_factory")
     moduleRegistry:requireAndRegisterObject("T_URL", "test.t_obj_url")
     moduleRegistry:requireAndRegisterObject("T_Block", "test.t_obj_block")
     moduleRegistry:requireAndRegisterObject("T_LayerRectangle", "test.t_obj_layer_rectangle")
