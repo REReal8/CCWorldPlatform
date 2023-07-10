fa37b84c609a026e4948e8ce401352babfc8fc6e
GuidoCram <50169830+GuidoCram@users.noreply.github.com>
2023-07-10 14:11:24 +0200

    Even better debug, still misses the call stack....

--- data/computercraft/lua/rom/modules/ccwp/obj/obj_host.lua
+++ data/computercraft/lua/rom/modules/ccwp/obj/obj_host.lua
@@ -215,7 +215,8 @@ function Host:getResource(...)
     -- get URL components
     local hostURI = resourceLocator:getHostURI()
     if hostURI ~= self:getHostName() then
-        corelog.Error("  Host:getResource: resourceLocator(="..resourceLocator:getURI()..") not for "..self:getHostName().." Host.")
+        corelog.Error("  Host:getResource(): resourceLocator not for host.")
+        corelog.WriteToLog("  resourceLocator:getURI() = " .. resourceLocator:getURI())
         corelog.WriteToLog("  hostURI = " .. hostURI)
         corelog.WriteToLog("  self:getHostName() = " .. self:getHostName())
         return nil
