ce5e7f03d8ac6d90334a26553aaf86f0b1976c45
Rutger van der Eijk <rutger.van.der.eijk@real8.nl>
2023-08-01 22:39:08 +0200

    Introduce ObjTable class

---
+++ data/computercraft/lua/rom/modules/ccwp/obj/obj_table.lua
@@ -0,0 +1,185 @@
+local ObjTable = {
+    _objClassName   = "",
+}
+
+local corelog = require "corelog"
+
+local InputChecker = require "input_checker"
+
+local IObj = require "i_obj"
+local ObjectFactory = require "object_factory"
+local objectFactory = ObjectFactory:getInstance()
+
+--[[
+    This module implements the class ObjTable.
+
+    A ObjTable is a key-value pair table of Obj's of the same 'class'. The class should implement the IObj interface.
+--]]
+
+--    _                                     _   _               _
+--   | |                                   | | | |             | |
+--   | |__   __ _ ___  ___   _ __ ___   ___| |_| |__   ___   __| |___
+--   | '_ \ / _` / __|/ _ \ | '_ ` _ \ / _ \ __| '_ \ / _ \ / _` / __|
+--   | |_) | (_| \__ \  __/ | | | | | |  __/ |_| | | | (_) | (_| \__ \
+--   |_.__/ \__,_|___/\___| |_| |_| |_|\___|\__|_| |_|\___/ \__,_|___/
+
+function ObjTable:new(...)
+    -- get & check input from description
+    local checkSuccess, o = InputChecker.Check([[
+        Constructs an ObjTable.
+
+        Parameters:
+            o                   + (table, {}) table with
+                _objClassName   - (string, "") with className of objects in ObjTable (e.g. "Chest")
+    ]], table.unpack(arg))
+    if not checkSuccess then corelog.Error("ObjTable:new: Invalid input") return nil end
+
+    -- set class info
+    setmetatable(o, self)
+    self.__index = self
+
+    -- transform Obj's if needed
+    o:transformObjectTables()
+
+    -- end
+    return o
+end
+
+function ObjTable:getClassName()
+    return "ObjTable"
+end
+
+function ObjTable:getObjClassName()
+    return self._objClassName
+end
+
+function ObjTable:getObjClass()
+    local objClass = objectFactory:getClass(self._objClassName)
+    if not objClass then corelog.Error("ObjTable:getObjClass(): failed obtaining class "..self._objClassName.." from objectFactory (did we forget to set _objClassName?)") end
+
+    return objClass
+end
+
+function ObjTable:isTypeOf(obj)
+    local metatable = getmetatable(obj)
+    while metatable do
+        if metatable.__index == self or obj == self then
+            return true
+        end
+        metatable = getmetatable(metatable.__index)
+    end
+    return false
+end
+
+function ObjTable:getNObjs()
+    -- count
+    local nObj = 0
+    for key, el in pairs(self) do
+        if key ~= "_objClassName" then
+            nObj = nObj + 1
+        end
+    end
+
+    -- end
+    return nObj
+end
+
+function ObjTable:isSame(obj)
+    -- check input
+    if not ObjTable:isTypeOf(obj) then return false end
+
+    -- check same class
+    if self._objClassName ~= obj._objClassName then return false end
+
+    -- check same elements as in A
+    local sizeA = 0
+    for key, elA in pairs(self) do
+        if key ~= "_objClassName"then
+            sizeA = sizeA + 1
+            -- check same obj
+            local elB = obj[key]
+            if not elA:isSame(elB) then return false end
+        end
+    end
+
+    -- count elements B
+    local sizeB = obj:getNObjs()
+
+    -- check same size
+    if sizeA ~= sizeB then return false end
+
+    -- end
+    return true
+end
+
+function ObjTable:copy()
+    -- create new ObjTable
+    local copy = ObjTable:new({
+        _objClassName   = self._objClassName,
+    })
+
+    -- copy elements
+    for key, el in pairs(self) do
+        if key ~= "_objClassName" then
+            copy[key] = el:copy()
+        end
+    end
+
+    return copy
+end
+
+--                        _  __ _                       _   _               _
+--                       (_)/ _(_)                     | | | |             | |
+--    ___ _ __   ___  ___ _| |_ _  ___   _ __ ___   ___| |_| |__   ___   __| |___
+--   / __| '_ \ / _ \/ __| |  _| |/ __| | '_ ` _ \ / _ \ __| '_ \ / _ \ / _` / __|
+--   \__ \ |_) |  __/ (__| | | | | (__  | | | | | |  __/ |_| | | | (_) | (_| \__ \
+--   |___/ .__/ \___|\___|_|_| |_|\___| |_| |_| |_|\___|\__|_| |_|\___/ \__,_|___/
+--       | |
+--       |_|
+
+function ObjTable:transformObjectTables(suppressWarning)
+    --[[
+        Transform the objects in the ObjTable that are still object tables into objects of type 'objClass'.
+
+        Parameters:
+            suppressWarning         + (boolean, false) if Warning should be suppressed
+    --]]
+    suppressWarning = suppressWarning or false
+
+    -- check if empty
+    if self:getNObjs() == 0 then return end
+
+    -- get objClass
+    local objClassName = self:getObjClassName()
+    local objClass = self:getObjClass()
+    if not objClass then corelog.Error("ObjTable:transformObjectTables(): failed obtaining objClass "..objClassName) return end
+    if not IObj.ImplementsInterface(objClass) then corelog.Error("ObjTable:transformObjectTables(): objClass "..objClassName.." does not implement IObj interface") return end
+
+    -- transform objectTable's
+    for key, objectTable in pairs(self) do
+        if key ~= "_objClassName" then
+            -- check if objectTable already an Obj
+            local obj = nil
+            if IObj.ImplementsInterface(objectTable) then
+                -- check Obj of correct type
+                if objClassName == objectTable:getClassName() then
+                    obj = objectTable -- already an object of type 'class'
+                else
+                    if not suppressWarning then corelog.Warning("ObjTable:transformObjectTables(): objectTable class (="..objectTable:getClassName()..") different from objClassName(="..objClassName..")") end
+                end
+            else
+                obj = objClass:new(objectTable) -- transform
+            end
+
+            -- add/ change in self ObjTable
+            self[key] = obj
+
+            -- check obj obtained
+            if not obj then
+                if not suppressWarning then corelog.Warning("ObjTable:transformObjectTables(): failed transforming objectTable(="..textutils.serialize(objectTable)..") to a "..objClassName.." object => skipped") end
+            end
+        end
+    end
+end
+
+return ObjTable
---
+++ data/computercraft/lua/rom/modules/ccwp/obj/test/t_obj_table.lua
@@ -0,0 +1,268 @@
+local T_ObjTable = {}
+
+local corelog = require "corelog"
+
+local IObj = require "i_obj"
+local ObjTable = require "obj_table"
+local TestObj = require "test.obj_test"
+local Location = require "obj_location"
+
+function T_ObjTable.T_All()
+    -- base methods
+    T_ObjTable.T_ImplementsIObj()
+    T_ObjTable.T_new()
+    T_ObjTable.T_isTypeOf()
+    T_ObjTable.T_isSame()
+    T_ObjTable.T_copy()
+
+    -- specific methods
+    T_ObjTable.T_transformObjectTables()
+    T_ObjTable.T_new_transformsObjTables()
+end
+
+local compact = { compact = true }
+
+--    _                                     _   _               _
+--   | |                                   | | | |             | |
+--   | |__   __ _ ___  ___   _ __ ___   ___| |_| |__   ___   __| |___
+--   | '_ \ / _` / __|/ _ \ | '_ ` _ \ / _ \ __| '_ \ / _ \ / _` / __|
+--   | |_) | (_| \__ \  __/ | | | | | |  __/ |_| | | | (_) | (_| \__ \
+--   |_.__/ \__,_|___/\___| |_| |_| |_|\___|\__|_| |_|\___/ \__,_|___/
+
+local objClassName1 = "TestObj"
+local testObj1 = TestObj:new({
+    _field1 = "field1_1",
+    _field2 = 1,
+})
+local testObj2 = TestObj:new({
+    _field1 = "field1_2",
+    _field2 = 2,
+})
+local wrongTestObj1 = Location:new()
+
+function T_ObjTable.T_ImplementsIObj()
+    -- prepare test
+    corelog.WriteToLog("* ObjTable IObj interface test")
+    local obj = ObjTable:new() if not obj then corelog.Error("failed obtaining ObjTable") return end
+
+    -- test
+    local implementsInterface = IObj.ImplementsInterface(obj)
+    assert(implementsInterface, "ObjTable class does not (fully) implement IObj interface")
+
+    -- cleanup test
+end
+
+function T_ObjTable.T_new()
+    -- prepare test
+    corelog.WriteToLog("* ObjTable:new() tests")
+
+    -- test full
+    local objTable = ObjTable:new({
+        _objClassName   = objClassName1,
+
+        testObj1Key     = testObj1,
+        testObj2Key     = testObj2,
+    }) if not objTable then corelog.Warning("objTable unexpectedly nil") return end
+    assert(objTable:getObjClassName() == objClassName1, "gotten getObjClassName(="..objTable:getObjClassName()..") not the same as expected(="..objClassName1..")")
+    local expectedNElements = 2
+    assert(objTable:getNObjs() == expectedNElements, " # elements(="..objTable:getNObjs()..") not the same as expected(="..expectedNElements..")")
+    assert(objTable.testObj1Key:isSame(testObj1), "obj 1 in objTable(="..textutils.serialise(objTable.testObj1Key, compact)..") not the same as expected(="..textutils.serialise(testObj1, compact)..")")
+    assert(objTable.testObj2Key:isSame(testObj2), "obj 2 in objTable(="..textutils.serialise(objTable.testObj2Key, compact)..") not the same as expected(="..textutils.serialise(testObj2, compact)..")")
+
+    -- test default
+    objTable = ObjTable:new() if not objTable then return end
+    local defaultName = ""
+    assert(objTable:getObjClassName() == defaultName, "gotten getObjClassName(="..objTable:getObjClassName()..") not the same as expected(="..defaultName..")")
+    expectedNElements = 0
+    assert(objTable:getNObjs() == expectedNElements, " # elements(="..objTable:getNObjs()..") not the same as expected(="..expectedNElements..")")
+
+    -- cleanup test
+end
+
+function T_ObjTable.T_isTypeOf()
+    -- prepare test
+    corelog.WriteToLog("* ObjTable:isTypeOf() tests")
+    local objTable2 = ObjTable:new({
+        _objClassName   = objClassName1,
+
+        testObj1,
+        testObj2,
+    })
+
+    -- test valid
+    local isTypeOf = ObjTable:isTypeOf(objTable2)
+    local expectedIsTypeOf = true
+    assert(isTypeOf == expectedIsTypeOf, "gotten isTypeOf(="..tostring(isTypeOf)..") not the same as expected(="..tostring(expectedIsTypeOf)..")")
+
+    -- test different object
+    isTypeOf = ObjTable:isTypeOf("a string")
+    expectedIsTypeOf = false
+    assert(isTypeOf == expectedIsTypeOf, "gotten isTypeOf(="..tostring(isTypeOf)..") not the same as expected(="..tostring(expectedIsTypeOf)..")")
+
+    -- cleanup test
+end
+
+function T_ObjTable.T_isSame()
+    -- prepare test
+    corelog.WriteToLog("* ObjTable:isSame() tests")
+    local objTable1 = ObjTable:new({
+        _objClassName   = objClassName1,
+
+        testObj1Key     = testObj1,
+        testObj2Key     = testObj2,
+    }) if not objTable1 then corelog.Warning("objTable1 unexpectedly nil") return end
+    local objTable2 = ObjTable:new({
+        _objClassName   = objClassName1,
+
+        testObj1Key     = testObj1,
+        testObj2Key     = testObj2,
+    })
+
+    -- test same
+    local isSame = objTable1:isSame(objTable2)
+    local expectedIsSame = true
+    assert(isSame == expectedIsSame, "gotten isSame(="..tostring(isSame)..") not the same as expected(="..tostring(expectedIsSame)..")")
+
+    -- test different className
+    local objClassName2 = "ProductionSpot"
+    objTable2._objClassName = objClassName2
+    isSame = objTable1:isSame(objTable2)
+    expectedIsSame = false
+    assert(isSame == expectedIsSame, "gotten isSame(="..tostring(isSame)..") not the same as expected(="..tostring(expectedIsSame)..")")
+    objTable2._objClassName = objClassName1
+
+    -- test different # objects
+    local testObj3 = testObj1:copy()
+    objTable2.testObj3Key = testObj3
+    isSame = objTable1:isSame(objTable2)
+    expectedIsSame = false
+    assert(isSame == expectedIsSame, "gotten isSame(="..tostring(isSame)..") not the same as expected(="..tostring(expectedIsSame)..")")
+    objTable2.testObj3Key = nil
+
+    -- cleanup test
+end
+
+function T_ObjTable.T_copy()
+    -- prepare test
+    corelog.WriteToLog("* ObjTable:copy() tests")
+    local objTable1 = ObjTable:new({
+        _objClassName   = objClassName1,
+
+        testObj1Key     = testObj1,
+        testObj2Key     = testObj2,
+    }) if not objTable1 then corelog.Warning("objTable1 unexpectedly nil") return end
+
+    -- test
+    local copy = objTable1:copy()
+    assert(copy:isSame(objTable1), "gotten copy(="..textutils.serialize(copy, compact)..") not the same as expected(="..textutils.serialize(objTable1, compact)..")")
+
+    -- cleanup test
+end
+
+--                        _  __ _                       _   _               _
+--                       (_)/ _(_)                     | | | |             | |
+--    ___ _ __   ___  ___ _| |_ _  ___   _ __ ___   ___| |_| |__   ___   __| |___
+--   / __| '_ \ / _ \/ __| |  _| |/ __| | '_ ` _ \ / _ \ __| '_ \ / _ \ / _` / __|
+--   \__ \ |_) |  __/ (__| | | | | (__  | | | | | |  __/ |_| | | | (_) | (_| \__ \
+--   |___/ .__/ \___|\___|_|_| |_|\___| |_| |_| |_|\___|\__|_| |_|\___/ \__,_|___/
+--       | |
+--       |_|
+
+function T_ObjTable.T_transformObjectTables()
+    -- prepare test
+    corelog.WriteToLog("* ObjTable:transformObjectTables() tests")
+    local objTable2 = ObjTable:new({
+        _objClassName   = objClassName1,
+    }) if not objTable2 then corelog.Warning("objTable2 unexpectedly nil") return end
+    local testObject1Table = {
+        _field1 = "field1_1",
+        _field2 = 1,
+    }
+    local testObject2Table = {
+        _field1 = "field1_2",
+        _field2 = 2,
+    }
+    assert(not TestObj:isTypeOf(testObject1Table), "testObject1Table incorrectly of type "..objClassName1)
+    assert(not TestObj:isTypeOf(testObject2Table), "testObject2Table incorrectly of type "..objClassName1)
+
+    -- test already only Obj's (nothing should change)
+    objTable2.testObj1Key = testObj1
+    objTable2.testObj2Key = testObj2
+    objTable2:transformObjectTables()
+    local expectedNElements = 2
+    assert(objTable2:getNObjs() == expectedNElements, " # elements(="..objTable2:getNObjs()..") not the same as expected(="..expectedNElements..")")
+    assert(objTable2.testObj1Key:isSame(testObj1), "obj 1 in array(="..textutils.serialise(objTable2.testObj1Key, compact)..") not the same as expected(="..textutils.serialise(testObj1, compact)..")")
+    assert(objTable2.testObj2Key:isSame(testObj2), "obj 2 in array(="..textutils.serialise(objTable2.testObj2Key, compact)..") not the same as expected(="..textutils.serialise(testObj2, compact)..")")
+    objTable2.testObj1Key = nil
+    objTable2.testObj2Key = nil
+
+    -- test different class Obj skipped
+    objTable2.testObj1Key = testObj1
+    objTable2.testObj3Key = wrongTestObj1
+    objTable2.testObj2Key = testObj2
+    objTable2:transformObjectTables(true)
+    expectedNElements = 2
+    assert(objTable2:getNObjs() == expectedNElements, " # elements(="..objTable2:getNObjs()..") not the same as expected(="..expectedNElements..")")
+    assert(objTable2.testObj1Key:isSame(testObj1), "obj 1 in array(="..textutils.serialise(objTable2.testObj1Key, compact)..") not the same as expected(="..textutils.serialise(testObj1, compact)..")")
+    assert(objTable2.testObj2Key:isSame(testObj2), "obj 2 in array(="..textutils.serialise(objTable2.testObj2Key, compact)..") not the same as expected(="..textutils.serialise(testObj2, compact)..")")
+    objTable2.testObj1Key = nil
+    objTable2.testObj2Key = nil
+    objTable2.testObj3Key = nil
+
+    -- test only object tables
+    objTable2.testObj1Key = testObject1Table
+    objTable2.testObj2Key = testObject2Table
+    objTable2:transformObjectTables()
+    assert(objTable2.testObj1Key:isSame(testObj1), "obj 1 in array(="..textutils.serialise(objTable2.testObj1Key, compact)..") not the same as expected(="..textutils.serialise(testObj1, compact)..")")
+    assert(objTable2.testObj2Key:isSame(testObj2), "obj 2 in array(="..textutils.serialise(objTable2.testObj2Key, compact)..") not the same as expected(="..textutils.serialise(testObj2, compact)..")")
+    objTable2.testObj1Key = nil
+    objTable2.testObj2Key = nil
+
+    -- test mix ObjTables and Obj's
+    testObject2Table = {
+        _field1 = "field1_2",
+        _field2 = 2,
+    }
+    assert(not TestObj:isTypeOf(testObject2Table), "testObject2Table incorrectly of type "..objClassName1)
+    objTable2.testObj1Key = testObj1
+    objTable2.testObj2Key = testObject2Table
+    objTable2:transformObjectTables()
+    assert(objTable2.testObj1Key:isSame(testObj1), "obj 1 in array(="..textutils.serialise(objTable2.testObj1Key, compact)..") not the same as expected(="..textutils.serialise(testObj1, compact)..")")
+    assert(objTable2.testObj2Key:isSame(testObj2), "obj 2 in array(="..textutils.serialise(objTable2.testObj2Key, compact)..") not the same as expected(="..textutils.serialise(testObj2, compact)..")")
+    objTable2.testObj1Key = nil
+    objTable2.testObj2Key = nil
+
+    -- cleanup test
+end
+
+function T_ObjTable.T_new_transformsObjTables()
+    -- prepare test
+    corelog.WriteToLog("* ObjTable:new() transforms objTables tests")
+    local testObject1Table = {
+        _field1 = "field1_1",
+        _field2 = 1,
+    }
+    local testObject2Table = {
+        _field1 = "field1_2",
+        _field2 = 2,
+    }
+
+    -- test new transforms objTables
+    local objTable = ObjTable:new({
+        _objClassName   = objClassName1,
+
+        testObject1Table,
+        testObject2Table,
+    })
+    if not objTable then return end
+    assert(objTable:getObjClassName() == objClassName1, "gotten getObjClassName(="..objTable:getObjClassName()..") not the same as expected(="..objClassName1..")")
+    local expectedNElements = 2
+    assert(table.getn(objTable) == expectedNElements, " # elements(="..table.getn(objTable)..") not the same as expected(="..expectedNElements..")")
+    local objClass = objTable:getObjClass()
+    assert(objClass:isTypeOf(objTable[1]), "obj 1 in objTable(="..textutils.serialise(objTable[1], compact)..") not of type "..objClassName1)
+    assert(objClass:isTypeOf(objTable[2]), "obj 2 in objTable(="..textutils.serialise(objTable[1], compact)..") not of type "..objClassName1)
+
+    -- cleanup test
+end
+
+return T_ObjTable
