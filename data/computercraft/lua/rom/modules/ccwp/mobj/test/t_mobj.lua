8ccd8dad9826a2bd8a2ed487d38c8e2b12b1d3cd
Rutger van der Eijk <rutger.van.der.eijk@real8.nl>
2023-08-07 00:11:53 +0200

    introduce T_MObj test helper

---
+++ data/computercraft/lua/rom/modules/ccwp/mobj/test/t_mobj.lua
@@ -0,0 +1,22 @@
+local T_MObj = {}
+local corelog = require "corelog"
+
+local T_Obj = require "test.t_obj"
+
+local compact = { compact = true }
+
+--    _____ __  __  ____  _     _                  _   _               _
+--   |_   _|  \/  |/ __ \| |   (_)                | | | |             | |
+--     | | | \  / | |  | | |__  _   _ __ ___   ___| |_| |__   ___   __| |___
+--     | | | |\/| | |  | | '_ \| | | '_ ` _ \ / _ \ __| '_ \ / _ \ / _` / __|
+--    _| |_| |  | | |__| | |_) | | | | | | | |  __/ |_| | | | (_) | (_| \__ \
+--   |_____|_|  |_|\____/|_.__/| | |_| |_| |_|\___|\__|_| |_|\___/ \__,_|___/
+--                            _/ |
+--                           |__/
+
+function T_MObj.T_ImplementsInterface(className, oTable)
+    assert(className, "no className provided")
+    T_Obj.ImplementsInterface("IMObj", className, oTable)
+end
+
+return T_MObj
