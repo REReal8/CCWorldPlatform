adb002eaeeeb26027953edd2ed6ae75ba8cf1b74
Rutger van der Eijk <rutger.van.der.eijk@real8.nl>
2023-08-07 03:26:25 +0200

    generalize provideItemsTo_AOSrv_Test and move it to T_ItemSupplier

--- data/computercraft/lua/rom/modules/ccwp/obj/test/t_item_supplier.lua
+++ data/computercraft/lua/rom/modules/ccwp/obj/test/t_item_supplier.lua
@@ -1,6 +1,13 @@
 local T_ItemSupplier = {}
 local corelog = require "corelog"
 
+local Callback = require "obj_callback"
+
+local URL = require "obj_url"
+local Host = require "obj_host"
+
+local enterprise_chests = require "enterprise_chests"
+
 local T_Obj = require "test.t_obj"
 
 local compact = { compact = true }
@@ -17,6 +24,60 @@ local compact = { compact = true }
 function T_ItemSupplier.T_ImplementsInterface(className, oTable)
     assert(className, "no className provided")
     T_Obj.ImplementsInterface("IItemSupplier", className, oTable)
+end
+
+function T_ItemSupplier.provideItemsTo_AOSrv_Test(mobjHostName, className, constructParameters, provideItems, itemDepotLocator)
+    -- prepare test (cont)
+    assert(mobjHostName, "no mobjHostName provided")
+    assert(className, "no className provided")
+    corelog.WriteToLog("* "..className..":provideItemsTo_AOSrv() test ("..textutils.serialize(provideItems, compact).." to "..itemDepotLocator:getURI()..")")
+
+    local mobjHost = Host.GetHost(mobjHostName) assert(mobjHost, "failed obtaining MObjHost "..mobjHostName)
+    local mobjLocator = mobjHost:hostMObj_SSrv({ className = className, constructParameters = constructParameters }).mobjLocator assert(mobjLocator, "failed hosting "..className.." on "..mobjHostName)
+    local mobj = mobjHost:getObject(mobjLocator) assert(mobj, "failed obtaining "..className.." from mobjLocator "..mobjLocator:getURI())
+
+    local expectedDestinationItemsLocator = itemDepotLocator:copy()
+    expectedDestinationItemsLocator:setQuery(provideItems)
+    local callback = Callback:new({
+        _moduleName     = "T_ItemSupplier",
+        _methodName     = "provideItemsTo_AOSrv_Callback",
+        _data           = {
+            ["expectedDestinationItemsLocator"] = expectedDestinationItemsLocator,
+            ["mobjHostName"]                    = mobjHostName,
+            ["mobjLocator"]                     = mobjLocator,
+            ["itemDepotLocator"]                = itemDepotLocator,
+        },
+    })
+
+    -- test
+    local scheduleResult = mobj:provideItemsTo_AOSrv({
+        provideItems    = provideItems,
+        itemDepotLocator= itemDepotLocator,
+    }, callback)
+    assert(scheduleResult == true, "failed to schedule async service")
+end
+
+function T_ItemSupplier.provideItemsTo_AOSrv_Callback(callbackData, serviceResults)
+    -- test (cont)
+    assert(serviceResults.success, "failed executing async service")
+
+    local destinationItemsLocator = URL:new(serviceResults.destinationItemsLocator)
+    local expectedDestinationItemsLocator = URL:new(callbackData["expectedDestinationItemsLocator"])
+    assert(destinationItemsLocator:isSame(expectedDestinationItemsLocator), "gotten destinationItemsLocator(="..textutils.serialize(destinationItemsLocator, compact)..") not the same as expected(="..textutils.serialize(expectedDestinationItemsLocator, compact)..")")
+
+    -- cleanup test
+    local mobjHostName = callbackData["mobjHostName"]
+    local mobjHost = Host.GetHost(mobjHostName) assert(mobjHost, "failed obtaining MObjHost "..mobjHostName)
+    local mobjLocator = callbackData["mobjLocator"]
+    mobjHost:releaseMObj_SSrv({ mobjLocator = mobjLocator})
+
+    local itemDepotLocator = callbackData["itemDepotLocator"]
+    if enterprise_chests:isLocatorFromHost(itemDepotLocator) then
+        enterprise_chests:deleteResource(itemDepotLocator)
+    end
+
+    -- end
+    return true
 end
 
 return T_ItemSupplier
