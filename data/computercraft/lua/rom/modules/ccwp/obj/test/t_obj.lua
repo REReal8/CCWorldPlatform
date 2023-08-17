85d37b035e7a60ac0ef8a1386678707446d3fa86
Rutger van der Eijk <rutger.van.der.eijk@real8.nl>
2023-08-17 16:48:53 +0200

    Update t_obj.lua

--- data/computercraft/lua/rom/modules/ccwp/obj/test/t_obj.lua
+++ data/computercraft/lua/rom/modules/ccwp/obj/test/t_obj.lua
@@ -3,14 +3,10 @@ local corelog = require "corelog"
 
 local ModuleRegistry = require "module_registry"
 local moduleRegistry = ModuleRegistry:getInstance()
-local ObjectFactory = require "object_factory"
-local objectFactory = ObjectFactory:getInstance()
 local Object = require "object"
 local IObj = require "i_obj"
 
-local compact = { compact = true }
-
--- ToDo: consider moving to more generic place (i.e. not coupled to IObj)
+-- ToDo: consider moving to more generic place for interfaces
 function T_Obj.pt_ImplementsInterface(interfaceName, className, obj)
     -- prepare test
     assert(className, "no className provided")
@@ -25,6 +21,7 @@ function T_Obj.pt_ImplementsInterface(interfaceName, className, obj)
     -- cleanup test
 end
 
+-- ToDo: consider moving to a new tests module for initialisation/ construction of objects, e.g. t_i_initialisation
 function T_Obj.pt_new(testClassName, obj, expectedFieldValues)
     -- prepare test
     assert(testClassName, "no className provided")
