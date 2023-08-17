a8a3471d84d8d62d719882ecd152791d858c9f6a
Rutger van der Eijk <rutger.van.der.eijk@real8.nl>
2023-08-17 09:01:32 +0200

    proper return for Async method

--- data/computercraft/lua/rom/modules/ccwp/mobj/mobj_mine.lua
+++ data/computercraft/lua/rom/modules/ccwp/mobj/mobj_mine.lua
@@ -34,9 +34,6 @@ local enterprise_turtle
 local enterprise_chests = require "enterprise_chests"
 local enterprise_storage
 
--- usefull
-local dthMineRoot   = "mobj_mine"
-
 --[[
     The following design decisions are made
         - The mine has full control of all area with z coordinate -32 and lower.
@@ -234,7 +231,7 @@ function Mine:provideItemsTo_AOSrv(...)
 
     -- start project
     corelog.WriteToLog(">Retrieve "..textutils.serialise(provideItems).." from Mine (!!NOT IMPLEMENTEND!!)")
-    return {success = false}
+    return Callback.ErrorCall(callback)
 end
 
 function Mine:can_ProvideItems_QOSrv(...)
