07a42fd08c607a1e7ce8b82ea7c46298f35bbf63
Rutger van der Eijk <rutger.van.der.eijk@real8.nl>
2023-09-27 09:58:15 +0200

    t_forestry: add -1 tests

--- data/computercraft/lua/rom/modules/ccwp/enterprise/test/t_forestry.lua
+++ data/computercraft/lua/rom/modules/ccwp/enterprise/test/t_forestry.lua
@@ -32,7 +32,10 @@ function t_forestry.T_AllPhysical()
     -- IObj methods
 
     -- MObjHost methods
-    local mobjLocator = t_forestry.T_hostAndBuildMObj_ASrv_BirchForest_L0T1()
+    local mobjLocator = t_forestry.T_hostAndBuildMObj_ASrv_BirchForest_Lm1T1()
+    t_forestry.T_extendAndUpgradeMObj_ASrv_BirchForestTo_L0T1(mobjLocator)
+    t_forestry.T_dismantleAndReleaseMObj_ASrv_BirchForest(mobjLocator)
+    mobjLocator = t_forestry.T_hostAndBuildMObj_ASrv_BirchForest_L0T1()
     t_forestry.T_dismantleAndReleaseMObj_ASrv_BirchForest(mobjLocator)
     mobjLocator = t_forestry.T_hostAndBuildMObj_ASrv_BirchForest_L1T2()
     t_forestry.T_dismantleAndReleaseMObj_ASrv_BirchForest(mobjLocator)
@@ -46,6 +49,7 @@ end
 
 local testMObjClassName = "BirchForest"
 local testMObjName = "birchForest"
+local testMObjNamem1 = testMObjName.."-1"
 local testMObjName0 = testMObjName.."0"
 local testMObjName1 = testMObjName.."1"
 local testMObjName2 = testMObjName.."2"
@@ -58,13 +62,18 @@ local level2 = 2
 local baseLocation0 = Location:newInstance(0, 0, 1, 0, 1)
 local nTrees1 = 1
 local nTrees2 = 2
-local nTrees3 = 3
 local nTrees4 = 4
 local localLogsLocator0 = enterprise_turtle.GetAnyTurtleLocator()
 local localSaplingsLocator0 = enterprise_turtle.GetAnyTurtleLocator()
 local localLogsLocatorTest0 = FieldValueEqualTest:newInstance("_localLogsLocator", localLogsLocator0)
 local localSaplingsLocatorTest0 = FieldValueEqualTest:newInstance("_localSaplingsLocator", localSaplingsLocator0)
 
+local constructParameters_Lm1T1 = {
+    level           = levelm1,
+
+    baseLocation    = baseLocation0,
+    nTrees          = nTrees1,
+}
 local constructParameters_L0T1 = {
     level           = level0,
 
@@ -83,133 +92,16 @@ local constructParameters_L2T2 = {
     baseLocation    = baseLocation0,
     nTrees          = nTrees2,
 }
+local upgradeParametersTo_L0T1 = {
+    level           = level0,
+
+    nTrees          = nTrees1,
+}
 local upgradeParametersTo_L2T4 = {
     level           = level2,
 
     nTrees          = nTrees4,
 }
-
-function t_forestry.T_AddNewSite_ASrv_Levelm1()
-    AddNewSite_ASrv(levelm1, nTrees1)
-end
-
-function AddNewSite_ASrv(level, nTrees)
-    -- prepare test
-    corelog.WriteToLog("# AddNewSite_ASrv (L"..level..",trees"..nTrees..") test")
-    local originalNForests = enterprise_forestry:getNumberOfObjects("BirchForest")
-
-    -- test
-    local serviceResults = MethodExecutor.DoASyncService_Sync("enterprise_forestry", "AddNewSite_ASrv", {
-        baseLocation                = baseLocation0:copy(),
-        forestLevel                 = level,
-        nTrees                      = nTrees,
-        materialsItemSupplierLocator= t_turtle.GetCurrentTurtleLocator(),
-        wasteItemDepotLocator       = t_turtle.GetCurrentTurtleLocator(),
-    })
-
-    -- check: service success
-    assert(serviceResults, "no serviceResults returned")
-    assert(serviceResults.success, "failed executing service")
-
-    -- check: Forest hosted on enterprise_forestry
-    local mobjLocator = serviceResults.mobjLocator assert(mobjLocator, "no mobjLocator returned")
-    local mobj = enterprise_forestry:getObject(mobjLocator)
-    assert(mobj, "Forest(="..mobjLocator:getURI()..") not hosted by "..enterprise_forestry:getHostName())
-
-    -- check: new forest added
-    local nForests = enterprise_forestry:getNumberOfObjects("BirchForest")
-    local expectedNForests = originalNForests + 1
-    assert(nForests == expectedNForests, "gotten nForests(="..nForests..") not the same as expected(="..expectedNForests..")")
-
-    -- cleanup test
-    mobj:destruct()
-    enterprise_forestry:deleteResource(mobjLocator)
-end
-
-function t_forestry.T_UpgradeSite_ASrv_Levelm1_Level0Trees1()
-    UpgradeSite_ASrv(levelm1, nTrees1, level0, nTrees1)
-end
-
-function t_forestry.ExtraCallback(...)
-    -- get & check input
-    local checkSuccess, levelWanted, treesWanted, mobjLocator = InputChecker.Check([[
-        Parameters:
-            extraCallbackData           - (table) callback data
-                levelWanted             + (number)
-                treesWanted             + (number)
-                mobjLocator             + (URL) locating the forest
-    --]], table.unpack(arg))
-    if not checkSuccess then corelog.Error("ExtraCallback: Invalid input") return end
-
-    -- call and test
-    CallAndTest_UpgradeSite_ASrv(mobjLocator, levelWanted, treesWanted, "", {})
-
-    -- end
-    return {success = true}
-end
-
-function UpgradeSite_ASrv(...)
-    -- get & check input
-    local checkSuccess, levelStart, treesStart, levelWanted, treesWanted, extraCallbackName, extraCallbackData = InputChecker.Check([[
-        Parameters:
-            levelStart              + (number)
-            treesStart              + (number)
-            levelWanted             + (number)
-            treesWanted             + (number)
-            extraCallbackName       + (string, "") extra callback method name
-            extraCallbackData       + (table, {}) extra callback data
-    --]], table.unpack(arg))
-    assert(checkSuccess, "UpgradeSite_ASrv: Invalid input")
-
-    -- prepare test
-    corelog.WriteToLog("# UpgradeSite_ASrv (L"..levelStart..",trees"..treesStart..") => (L"..levelWanted..",trees"..treesWanted..") test")
-    local serviceResults = MethodExecutor.DoASyncService_Sync("enterprise_forestry", "AddNewSite_ASrv", {
-        baseLocation                = baseLocation0:copy(),
-        forestLevel                 = levelStart,
-        nTrees                      = treesStart,
-        materialsItemSupplierLocator= t_turtle.GetCurrentTurtleLocator(),
-        wasteItemDepotLocator       = t_turtle.GetCurrentTurtleLocator(),
-    })
-    assert(serviceResults, "failed preparing test")
-    assert(serviceResults.success, "failed executing service")
-    local mobjLocator = URL:new(serviceResults.mobjLocator) assert(mobjLocator, "no mobjLocator returned")
-
-    -- call and test
-    CallAndTest_UpgradeSite_ASrv(mobjLocator, levelWanted, treesWanted, extraCallbackName, extraCallbackData)
-end
-
-function CallAndTest_UpgradeSite_ASrv(mobjLocator, levelWanted, treesWanted, extraCallbackName, extraCallbackData)
-    -- test
-    local serviceResults = MethodExecutor.DoASyncService_Sync("enterprise_forestry", "UpgradeSite_ASrv", {
-        mobjLocator                 = mobjLocator:copy(),
-        targetLevel                 = levelWanted,
-        targetNTrees                = treesWanted,
-        materialsItemSupplierLocator= t_turtle.GetCurrentTurtleLocator(),
-        wasteItemDepotLocator       = t_turtle.GetCurrentTurtleLocator(),
-    })
-
-    -- check: service success
-    assert(serviceResults, "failed upgrading Forest")
-    assert(serviceResults.success, "failed executing service")
-
-    -- check: Forest
-    local forest = enterprise_forestry:getObject(mobjLocator)
-    assert(forest:getLevel() == levelWanted, "gotten level(="..forest:getLevel()..") not the same as expected(="..levelWanted..")")
-    assert(forest:getNTrees() == treesWanted, "gotten nTrees(="..forest:getNTrees()..") not the same as expected(="..treesWanted..")")
-
-    -- extraCallback?
-    if extraCallbackName == "" then
-        -- cleanup test
-        local mobj = enterprise_forestry:getObject(mobjLocator) assert(mobj, "Forest(="..mobjLocator:getURI()..") not hosted by "..enterprise_forestry:getHostName())
-        mobj:destruct()
-        enterprise_forestry:deleteResource(mobjLocator)
-    else
-        -- call extraCallback
-        extraCallbackData.mobjLocator = mobjLocator
-        local moduleName = "t_forestry"
-        MethodExecutor.DoSyncService(moduleName, extraCallbackName, extraCallbackData)
-    end
-end
 
 --    __  __  ____  _     _ _    _           _                    _   _               _
 --   |  \/  |/ __ \| |   (_) |  | |         | |                  | | | |             | |
@@ -232,6 +124,20 @@ function t_forestry.T_hostMObj_SSrv_BirchForest()
 end
 
 local mobjLocator_BirchForest = nil
+
+function t_forestry.T_hostAndBuildMObj_ASrv_BirchForest_Lm1T1()
+    -- prepare test
+
+    -- test
+    local serviceResults = T_MObjHost.pt_hostAndBuildMObj_ASrv(enterprise_forestry, testMObjClassName, constructParameters_Lm1T1, testMObjNamem1, logOk)
+    assert(serviceResults, "no serviceResults returned")
+
+    -- cleanup test
+    mobjLocator_BirchForest = serviceResults.mobjLocator
+
+    -- return mobjLocator
+    return serviceResults.mobjLocator
+end
 
 function t_forestry.T_hostAndBuildMObj_ASrv_BirchForest_L0T1()
     -- prepare test
@@ -293,6 +199,24 @@ function t_forestry.T_upgradeMObj_SSrv_BirchForest()
 
     -- cleanup test
 end
+
+function t_forestry.T_extendAndUpgradeMObj_ASrv_BirchForestTo_L0T1(mobjLocator)
+    -- prepare test
+    local upgradeFieldsTest = T_BirchForest.CreateInitialisedTest(nil, level0, baseLocation0, nTrees1, localLogsLocatorTest0, localSaplingsLocatorTest0)
+
+    if not mobjLocator then
+        -- check if we locally remembered a mobjLocator
+        assert(mobjLocator_BirchForest, "no mobjLocator to operate on")
+        mobjLocator = mobjLocator_BirchForest
+    end
+
+    -- test
+    local serviceResults = T_MObjHost.pt_extendAndUpgradeMObj_ASrv(enterprise_forestry, mobjLocator, upgradeParametersTo_L0T1, mobjLocator:getURI(), upgradeFieldsTest, logOk)
+    assert(serviceResults, "no serviceResults returned")
+
+    -- cleanup test
+end
+
 
 function t_forestry.T_extendAndUpgradeMObj_ASrv_BirchForestTo_L2T4(mobjLocator)
     -- prepare test
