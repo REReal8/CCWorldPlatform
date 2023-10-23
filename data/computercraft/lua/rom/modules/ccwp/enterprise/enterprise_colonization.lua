c03791f8f2244ddcf832a4dec1532fb018954cb6
Rutger van der Eijk <rutger.van.der.eijk@real8.nl>
2023-10-24 00:34:57 +0200

    enterprise_colonization: delist Turtle from Shop after it created the new world

--- data/computercraft/lua/rom/modules/ccwp/enterprise/enterprise_colonization.lua
+++ data/computercraft/lua/rom/modules/ccwp/enterprise/enterprise_colonization.lua
@@ -307,9 +307,16 @@ function enterprise_colonization.CreateNewWorld_ASrv(...)
             { stepType = "SSrv", stepTypeDef = { moduleName = "enterprise_shop", serviceName = "RegisterItemSupplier_SSrv" }, stepDataDef = {
                 { keyDef = "itemSupplierLocator"            , sourceStep = 22, sourceKeyDef = "mobjLocator" },
             }},
+            -- delist current Turtle from Shop
+            { stepType = "SSrv", stepTypeDef = { moduleName = "enterprise_shop", serviceName = "DelistItemSupplier_SSrv" }, stepDataDef = {
+                { keyDef = "itemSupplierLocator"            , sourceStep = 3, sourceKeyDef = "mobjLocator" },
+            }},
+            --
             { stepType = "SSrv", stepTypeDef = { moduleName = "enterprise_dump", serviceName = "ListItemDepot_SSrv" }, stepDataDef = {
                 { keyDef = "itemDepotLocator"               , sourceStep = 22, sourceKeyDef = "mobjLocator" },
             }},
+            -- dump all gathered waste from current Turtle
+            -- ToDo: implement
             -- update enterprise_energy to L3 (refuelAmount = 6 trees = 1800, fuelNeed_Refuel = fuelNeed_Harvest + fuelNeed_ExtraTree + fuelNeed_Production + fuelNeed_ForestFactoryTravel)
             { stepType = "SSrv", stepTypeDef = { moduleName = "enterprise_energy", serviceName = "UpdateEnterprise_SSrv" }, stepDataDef = {
                 { keyDef = "enterpriseLevel"            , sourceStep = 0, sourceKeyDef = "energyL3" },
