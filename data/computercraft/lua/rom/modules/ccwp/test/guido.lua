e60ca132080deec2c30dc1eefc546c0df52cb8d5
GuidoCram <50169830+GuidoCram@users.noreply.github.com>
2023-11-01 19:29:52 +0100

    new file for new idear (just testing the idear at the moment)

---
+++ data/computercraft/lua/rom/modules/ccwp/test/guido.lua
@@ -0,0 +1,25 @@
+-- core stuff
+local coresystem        = require "coresystem"
+local coreevent         = require "coreevent"
+local corelog           = require "corelog"
+local coredisplay       = require "coredisplay"
+local coredht           = require "coredht"
+local coreinventory     = require "coreinventory"
+local coreassignment    = require "coreassignment"
+local coreutils         = require "coreutils"
+local coremove          = require "coremove"
+local coretask          = require "coretask"
+
+-- enterprises
+local enterprise_forestry   = require "enterprise_forestry"
+
+-- mobj's
+
+-- obj's
+
+-- roles
+local role_forester     = require "role_forester"
+
+-- testing
+print('Hello world!')
+corelog.WriteToLog('Hello world!')
